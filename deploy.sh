fe1083ca266e429778f69c8fe14b21515445fbca
Revathi <revathi@Revathis-MacBook-Pro.local>
2020-10-15 13:11:23 +0530

    added script for deploy

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+rm -rf * .*
+
+echo 'Cloning backend...'
+git clone  https://github.com/${USER_NAME}/${BACKEND}.git 2> /dev/null
+cd ${BACKEND}
+
+echo 'Installing dependencies...'
+npm install 2> /dev/null
+npm test
+cd ..
+
+echo 'Cloning frontend ...'
+git clone https://github.com/${USER_NAME}/${FRONTEND}.git 2> /dev/null
+cd ${FRONTEND}
+
+echo 'Installing dependencies...'
+npm install 2> /dev/null
+npm test
+
+echo 'Creating build ...'
+npm run build 2> /dev/null
+
+echo 'Merging build ...'
+mkdir -p ../public
+mv build/* ../public/.
+cd ..
+mv ${BACKEND}/* ${BACKEND}/.* .
+
+echo 'Removing un-neccessaries...'
+rm -rf ${FRONTEND}
+rm -rf ${BACKEND}
+
+echo 'Process completed...'
